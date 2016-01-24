77e79baf8fa16cebcf2d9f42061ba6f3cdbb0613
Moiz Qureshi <moquresh@ucsd.edu>
2016-01-24 14:54:39 -0800

    Implemented printChar.s; needs to be tested

---
+++ printChar.s
@@ -0,0 +1,45 @@
+/*
+ * Filename: printChar.s
+ * Author: Moiz Qureshi
+ * Userid: cs30xix
+ * Description: Prints char passed in as argument to stdout. Just prints a 
+ * 		single character
+ * Date: 1/24/2016
+ * Sources of help: PA1 Assignment Sheet, PA1 Discussion Slides Pt.2,
+ * 		    PA0 Code Handout (printWelcome.s)
+ */
+
+	.global printChar	! Declares the symbol to be globally visible
+				! so we can can call this function from
+				! other modules
+	
+	.section ".text"	! The text segment begins here
+
+/*
+ * Function name: printChar()
+ * Function prototype: void printChar( char ch );
+ * Description: Prints one char at a time, char is argument that is passed in
+ * Parameters:
+ * 	arg 1: char ch -- char to be printed once
+ * Side Effects: Just prints char ch once
+ * Error Conditions: None (input error checking done in pa1.c)
+ * Return Values: None
+ *
+ * Registers Used:
+ * 	%i0 - arg 1 -- char ch
+ */
+
+printChar:
+	save	%sp, -96, %sp	! Save the caller's window; if different than
+				! -96, then comment on how that value was 
+				! calculated
+	
+	mov 	%i0, %o0	! Copy of parameter to print
+	call 	printf		! Make printf function call
+	nop			! Delay slot for call instruction
+
+	ret			! Return from subroutine
+	restore			! Restore caller's window; in "ret" delay slot
+
+
+
