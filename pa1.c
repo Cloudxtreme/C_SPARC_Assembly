d38eee4c9392348a37d60f003e6de51d6f609d1b
Moiz Qureshi <moquresh@ucsd.edu>
2016-01-26 00:05:05 -0800

    pa1.c - Completed checking of number of arguments

--- pa1.c
+++ pa1.c
@@ -41,9 +41,69 @@
  */
 
 int
-main( void ) {
+main( int argc, char *argv[]  ) {
+  
+  // Local main() variable defines
+  int integer_failure = 0;
+  
+  long xorChar = 0;   // var to hold xorChar when converted from string
+  long width = 0;     // var to hold width when converted from string
+  long speed = 0;     // var to hold speed when converted from string
+  
+  char *testptr = NULL;
 
-  char x = 'Z';
-  drawXOR( x, 3, 100000 );
-  return 0;
+  /* Check to see number of arguments passed is equal to 4, otherwise print
+  *  usage error message to stderr and exit program with failure
+  */
+  if ( argc != NUM_ARGV ) {
+    (void) fprintf( stderr, USAGE );
+    return EXIT_FAILURE;
+  }
+  
+  /* Convert all argument to type long, setting their respective variables
+  *  equal to them, then check to see if the original argument is an integer
+  *  by checking testptr to see if it is null or not, otherwise print integer
+  *  error message to stderr and exit program with failure
+  */
+  xorChar = strtol( argv[1], &testptr, BASE );
+  if ( testptr != NULL ) {
+    (void) fprintf( stderr, INT_ERR, argv[1] );
+    integer_failure = 1;
+    testptr = NULL;
+  }
+
+  width = strtol( argv[2], &testptr, BASE );
+  if ( testptr != NULL ) {
+    (void) fprintf( stderr, INT_ERR, argv[2] );
+    integer_failure = 1;
+    testptr = NULL;
+  }
+
+  speed = strtol( argv[3], &testptr, BASE );
+  if ( testptr != NULL ) {
+    (void) fprintf( stderr, INT_ERR, argv[3] );
+    integer_failure = 1;
+    testptr = NULL;
+  }
+
+  if ( integer_failure == 1) {
+    return EXIT_FAILURE;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  drawXOR( xorChar, width, speed );
+  return EXIT_SUCCESS;
 }
