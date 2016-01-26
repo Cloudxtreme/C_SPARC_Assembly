bd058694e43eec8b8041b326688b8962e4e59332
Moiz Qureshi <moquresh@ucsd.edu>
2016-01-26 12:28:45 -0800

    Completed and tested pa1.c

--- pa1.c
+++ pa1.c
@@ -44,13 +44,17 @@ int
 main( int argc, char *argv[]  ) {
   
   // Local main() variable defines
-  int integer_failure = 0;
+  int strtol_fail = 0;          // boolean for strtol fail
+  int xorChar_range_fail = 1;   // boolean for xorChar range fail
+  int width_range_fail = 1;     // boolean for width range fail
+  int speed_range_fail = 1;     // boolean for speed range fail
+  int width_odd_fail = 1;       // boolean for width odd fail
   
   long xorChar = 0;   // var to hold xorChar when converted from string
   long width = 0;     // var to hold width when converted from string
   long speed = 0;     // var to hold speed when converted from string
   
-  char *testptr = NULL;
+  char *testptr = NULL;   // test pointer used when doing strtol conversion
 
   /* Check to see number of arguments passed is equal to 4, otherwise print
   *  usage error message to stderr and exit program with failure
@@ -59,51 +63,102 @@ main( int argc, char *argv[]  ) {
     (void) fprintf( stderr, USAGE );
     return EXIT_FAILURE;
   }
-  
-  /* Convert all argument to type long, setting their respective variables
-  *  equal to them, then check to see if the original argument is an integer
-  *  by checking testptr to see if it is null or not, otherwise print integer
-  *  error message to stderr and exit program with failure
-  */
+
+  /* Convert 1st argument string into long and set xorChar equal to it,
+   * and check to see if argument was an integer by inspecting testptr,
+   * otherwise print integer error. Then use checkRange assembly function 
+   * to see if xorChar is within range, otherwise print the range error,
+   * and set xorChar range boolean appropriately. 
+   */
   xorChar = strtol( argv[1], &testptr, BASE );
-  if ( testptr != NULL ) {
+  if ( *testptr == NULL ) {
+    xorChar_range_fail = checkRange( MIN_XOR_CHAR, OFFSET_XOR_CHAR, xorChar );
+    if (xorChar_range_fail == 0 || xorChar_range_fail == -1 ) {
+      (void) fprintf( stderr, XOR_ERR, xorChar, MIN_XOR_CHAR, 
+      MIN_XOR_CHAR+OFFSET_XOR_CHAR );
+    }
+  }
+  else {
     (void) fprintf( stderr, INT_ERR, argv[1] );
-    integer_failure = 1;
-    testptr = NULL;
+    strtol_fail = 1;
+    *testptr = NULL;
   }
 
+  /* Convert 2nd argument string into long and set width equal to it, and
+   * check to see if argument was an integer by inspecting testptr, 
+   * otherwise print integer error. Then use checkRange assembly function
+   * to see if width is within range, otherwise print range error, and
+   * set width range boolean appropriately. Then also check to see if,
+   * width is odd by calling isOdd assembly function, otherwise print
+   * width odd error and set width odd boolean appropriately.
+   */
   width = strtol( argv[2], &testptr, BASE );
-  if ( testptr != NULL ) {
+  if ( *testptr == NULL ) { 
+    width_range_fail = checkRange( MIN_WIDTH, OFFSET_WIDTH, width );
+    if ( width_range_fail == 1 ) { 
+      width_odd_fail = isOdd( width );
+      if ( width_odd_fail == 0 ) {
+        (void) fprintf( stderr, WIDTH_ODD_ERR, width );
+      }
+    }
+    else { 
+       (void) fprintf( stderr, WIDTH_ERR, width, MIN_WIDTH,
+       MIN_WIDTH+OFFSET_WIDTH );
+    }
+  }  
+  else {
     (void) fprintf( stderr, INT_ERR, argv[2] );
-    integer_failure = 1;
-    testptr = NULL;
+    strtol_fail = 1;
+    *testptr = NULL;
   }
 
+  /* Convert 3rd argument string into long and set speed equal to it, and
+   * check to see if argument was an integer by inspecting testptr, 
+   * otherwise print integer error. Then use checkRange assembly function
+   * to see if speed is within range, otherwise print range error, and
+   * set speed range boolean appropriately.
+   */
   speed = strtol( argv[3], &testptr, BASE );
-  if ( testptr != NULL ) {
+  if ( *testptr == NULL ) {
+    speed_range_fail = checkRange( MIN_SPEED, OFFSET_SPEED, speed );
+    if ( speed_range_fail == 0 || speed_range_fail == -1 ) {
+      (void) fprintf( stderr, SPEED_ERR, speed, MIN_SPEED, 
+      MIN_SPEED+OFFSET_SPEED );
+    }
+  }
+  else {
     (void) fprintf( stderr, INT_ERR, argv[3] );
-    integer_failure = 1;
-    testptr = NULL;
+    strtol_fail = 1;
+    *testptr = NULL;
   }
 
-  if ( integer_failure == 1) {
+  // Check strtol failure boolean to exit program with failure or not
+  if ( strtol_fail == 1) {
+    return EXIT_FAILURE;
+  }
+  
+  // Check range failure booleans to exit program with failure or not
+  if ( xorChar_range_fail == 0 || xorChar_range_fail == -1 ) {
     return EXIT_FAILURE;
   }
 
+  if ( width_range_fail == 0 || width_range_fail == -1 ) {
+    return EXIT_FAILURE;
+  }
 
+  if ( speed_range_fail == 0 || speed_range_fail == -1 ) {
+    return EXIT_FAILURE;
+  }
 
+  // Check width odd failure boolean to exit program with failure or not
+  if ( width_odd_fail == 0 ) {
+    return EXIT_FAILURE;
+  }
 
-
-
-
-
-
-
-
-
-
-
-
+  // Since no errors were encountered by now, draw the XOR pattern
+  // by calling drawXOR assembly function.
   drawXOR( xorChar, width, speed );
+
+  // Exit program with sucesss!!!! Hooray! Yippee!
   return EXIT_SUCCESS;
 }
--- pa1Strings.h
+++ pa1Strings.h
@@ -1,14 +1,3 @@
-/*
- * Filename: pa1Strings.h
- * Author: CSE30 Tutors - Given file
- * Userid: cs30xix
- * Description: String defines for PA1
- * Date: 1/24/16
- * Sources of Help: This is a given, copied over file from public
- */
-
-#ifndef PA1_STRINGS_H
-#define PA1_STRINGS_H
 #define USAGE "\nUsage: ./pa1 xorChar width speed\n\
   xorChar (must be an ASCII value within the range of [33 - 126])\n\
   width   (must be an numerical value within the range [3 - 39])\n\
@@ -23,5 +12,3 @@
 #define WIDTH_ERR "\n    width(%lu) (must be within the range [%u - %u])\n\n"
 #define WIDTH_ODD_ERR "\n    width(%lu) (must be an odd integer)\n\n"
 #define SPEED_ERR "\n    speed(%lu) (must be within the range [%u - %u])\n\n"
-
-#endif /* PA1_STRINGS_H */
