4530332dde220dfd9f3d3a87a651d1d31f95a0ee
Moiz Qureshi <moquresh@ucsd.edu>
2016-02-07 23:52:38 -0800

    displayBCDClock.c - Removed nested for loop since it was not needed, fixed incorrect logic with &'ing repeatedly of temp char vars

--- displayBCDClock.c
+++ displayBCDClock.c
@@ -13,6 +13,7 @@
  */
 #include <stdio.h>
 #include "pa2.h"
+#include "pa2Strings.h"
 
 /*
  * Function name: displayBCDClock()
@@ -38,8 +39,7 @@ void
 displayBCDClock( const unsigned char clockBCD[] ) {
 
   // Define Local variables
-  int i = 0;      // Used as counter in outer for loop
-  int j = 0;      // Used as counter in inner for loop
+  int i = 0;      // Used as counter in for loop
 
   /* Get the ones and tesns mask values from defines in pa2.h,
    * doing this since we will be changing the mask in the program.
@@ -48,85 +48,81 @@ displayBCDClock( const unsigned char clockBCD[] ) {
   int tens_mask = TENS_MSK;
 
   // Temp unsigned chars to values of clock
-  unsigned char hours_tmp = clockBCD[HR_INDEX];
-  unsigned char mins_tmp = clockBCD[MIN_INDEX];
-  unsigned char secs_tmp = clockBCD[SEC_INDEX];
+  unsigned char hours_tmp;
+  unsigned char mins_tmp;
+  unsigned char secs_tmp;
   
-  /* Outer loop is for each row of clock, inner loop prints each row */
+  /* Loop is for each row of clock */
   for( i; i < ROWS; i++ ) {
-    for( j; j < NIB_LEN; j++ ) {
-      /* Get tens place mask result of hours MSB, and check if it is 1 or 0,
-       * then print the appropriate character, followed by printing a space
-       */
-      hours_tmp = hours_tmp & tens_mask;
-      if( hours_tmp ) {
-        printChar( ONE_CHAR );
-      } else {
-        printChar( ZERO_CHAR );
-      }
-      printChar( SPACE );
-
-      /* Get ones place mask result of hours MSB, and check if it is 1 or 0,
-       * then print the appropriate character, followed by printing a space
-       */
-      hours_tmp = hours_tmp & ones_mask
-      if( hours_tmp ) {
-        printChar( ONE_CHAR );
-      } else {
-        printChar( ZERO_CHAR );
-      }
-      printChar( SPACE );
-
-      /* Get tens place mask result of mins MSB, and check if it is 1 or 0,
-       * then print the appropriate character, followed by printing a space
-       */
-      mins_tmp = mins_tmp & tens_mask;
-      if( mins_tmp ) {
-        printChar( ONE_CHAR );
-      } else {
-        printChar( ZERO_CHAR );
-      }
-      printChar( SPACE );
-
-      /* Get ones place mask result of mins MSB, and check if it is 1 or 0,
-       * then print the appropriate character, followed by printing a space
-       */
-      mins_tmp = mins_tmp & ones_mask
-      if( mins_tmp ) {
-        printChar( ONE_CHAR );
-      } else {
-        printChar( ZERO_CHAR );
-      }
-      printChar( SPACE );
-
-      /* Get tens place mask result of secs MSB, and check if it is 1 or 0,
-       * then print the appropriate character, followed by printing a space
-       */
-      secs_tmp = secs_tmp & tens_mask;
-      if( secs_tmp ) {
-        printChar( ONE_CHAR );
-      } else {
-        printChar( ZERO_CHAR );
-      }
-      printChar( SPACE );
-
-      /* Get ones place mask result of secs MSB, and check if it is 1 or 0,
-       * then print the appropriate character, followed by printing a space
-       */
-      secs_tmp = secs_tmp & ones_mask;
-      if( secs_tmp ) {
-        printChar( ONE_CHAR );
-      } else {
-        printChar( ZERO_CHAR );
-      }
-      printChar( SPACE );
-      
-      /* Now we will shift the mask 1 bit left to get next binary digit of
-       * nibble, when we run thru this loop next time.
-       */
-       tens_mask = tens_mask >> 1;
-       ones_mask = ones_mask >> 1;
+    /* Get tens place mask result of hours MSB, and check if it is 1 or 0,
+     * then print the appropriate character, followed by printing a space
+     */
+    hours_tmp = clockBCD[HR_INDEX] & tens_mask;
+    if( hours_tmp ) {
+      printChar( ONE_CHAR );
+    } else {
+      printChar( ZERO_CHAR );
     }
+    
+    /* Get ones place mask result of hours MSB, and check if it is 1 or 0,
+     * then print the appropriate character, followed by printing a space
+     */
+    hours_tmp = clockBCD[HR_INDEX] & ones_mask;
+    if( hours_tmp ) {
+      printChar( ONE_CHAR );
+    } else {
+      printChar( ZERO_CHAR );
+    }
+    printChar( SPACE );
+
+    /* Get tens place mask result of mins MSB, and check if it is 1 or 0,
+     * then print the appropriate character, followed by printing a space
+     */
+    mins_tmp = clockBCD[MIN_INDEX] & tens_mask;
+    if( mins_tmp ) {
+      printChar( ONE_CHAR );
+    } else {
+      printChar( ZERO_CHAR );
+    }
+    
+    /* Get ones place mask result of mins MSB, and check if it is 1 or 0,
+     * then print the appropriate character, followed by printing a space
+     */
+    mins_tmp = clockBCD[MIN_INDEX] & ones_mask;
+    if( mins_tmp ) {
+      printChar( ONE_CHAR );
+    } else {
+      printChar( ZERO_CHAR );
+    }
+    printChar( SPACE );
+
+    /* Get tens place mask result of secs MSB, and check if it is 1 or 0,
+     * then print the appropriate character, followed by printing a space
+     */
+    secs_tmp = clockBCD[SEC_INDEX] & tens_mask;
+    if( secs_tmp ) {
+      printChar( ONE_CHAR );
+    } else {
+      printChar( ZERO_CHAR );
+    }
+    
+    /* Get ones place mask result of secs MSB, and check if it is 1 or 0,
+     * then print the appropriate character, followed by printing a space
+     */
+    secs_tmp = clockBCD[SEC_INDEX] & ones_mask;
+    if( secs_tmp ) {
+      printChar( ONE_CHAR );
+    } else {
+      printChar( ZERO_CHAR );
+    }
+    printChar( SPACE );
+
+    /* Now we will shift the mask 1 bit left to get next binary digit of
+     * nibble, when we run thru this loop next time.
+     */
+    tens_mask = tens_mask >> 1;
+    ones_mask = ones_mask >> 1;
+
     /* Print newline after each row */
     printChar( NEWLINE );
   }
@@ -135,4 +131,5 @@ displayBCDClock( const unsigned char clockBCD[] ) {
   printf( STR_BCD_TIME, clockBCD[HR_INDEX] );
   printf( STR_BCD_TIME, clockBCD[MIN_INDEX] );
   printf( STR_BCD_TIME, clockBCD[SEC_INDEX] );
+  printChar( NEWLINE );
 }
