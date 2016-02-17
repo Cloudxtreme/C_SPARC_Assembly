1593e384cd273031fb6709a55c0ad6ba4e8b6a1e
Moiz Qureshi <moquresh@ucsd.edu>
2016-02-16 23:20:49 -0800

    hashString.s - Implemented & Commented; passes basic unit tests, need more cases

--- hashString.s
+++ hashString.s
@@ -52,6 +52,8 @@
  * 	%l2 -- int hash
  * 	%l3 -- int strLen
  * 	%l4 -- int i (for-loop counter)
+ * 	%l5 -- char *str
+ * 	%l6 -- char str[i] 
  */
 
 hashString:
@@ -64,25 +66,59 @@ hashString:
 	clr 	%l2		! Clear %l2 register
 	clr	%l3		! Clear %l3 register
 	clr 	%l4		! Clear %l4 register
+	clr 	%l5		! Clear %l5 register
+	clr	%l6		! Clear %l6 register
 	
-	set	HashStartVal, %l0	! Copy value of HashStartVal itno %l0 
-	set 	HashPrime, %l1		! Copy value of HashPrime into %l1
+	set	HashStartVal, %l0	! Copy value of HashStartVal into %l0 
+	ld	[%l0], %l0
 
+	set	HashPrime, %l1		! Copy value of HashPrime into %l1
+	ld 	[%l1], %l1		
+
+	mov	%i0, %l5	! Copy value of *str into %l5
 	mov	%l0, %l2	! Intialize value of hash to HashStartVal
 
-	mov	%i0, %o0	! Copy *str into %o0, so we can call strlen().
+	mov	%l5, %o0	! Copy *str into %o0, so we can call strlen().
 	call	strlen
 	nop
 
-	mov	%o0, %l2	! Copy result of strlen(), into %l2
+	mov	%o0, %l3	! Copy result of strlen(), into %l3
 
-	cmp	%l4, %l3	! If 
-	bge	endloop
-	nop
+	cmp	%l4, %l3	! If i is greater than or equal to strLen, then
+	bge	endloop		! branch to endloop, otherwise continue to 
+	nop			! loop branch
 
 loop:
+	mov 	%l2, %o0	! Find product of hash*HashPrime  
+	mov	%l1, %o1	
+	call	.mul
+	nop
+
+	mov	%o0, %l2 	! Store result of hash*HashPrime back into %l2
+	ldub	[%l5], %l6	! Get value of str[i]
+	add     %l2, %l6, %l2   ! Add str[i] to hash*HashPrime	
 	
+	inc 	%l5		! Increment *str by 1 
+	inc 	%l4		! Increment i by 1
 
 
+	cmp 	%l4, %l3	! If i < strLen, then continue looping, 
+	bl	loop		! otherwise continue below to endloop
+	nop
 
 endloop:
+	mov 	%l2, %i0	! Copy hash value into %i0, so we can return 
+				! this value from this function
+
+	ret			! Return from subroutine	
+	restore			! Restore caller's window; in ret's delay slot
+
+
+
+
+
+
+
+
+
+
