17a744b7e66c42d7f94b84b858f0dbf909b6f9eb
Moiz Qureshi <moquresh@ucsd.edu>
2016-02-20 16:05:12 -0800

    pa3Strings.h - new version from public dir

--- pa3Strings.h
+++ pa3Strings.h
@@ -12,8 +12,11 @@
 #define STR_DEF_ANAGRAM_DB "anagram_database.db"
 #define STR_DB_BUILT "Anagram database file \"%s\" built.\n"
 #define STR_USER_PROMPT "\nEnter a word to search for anagrams [^D to exit]: "
+#define STR_NO_ANAGRAMS "No anagrams found.\n"
+#define STR_FOUND_ANAGRAMS "Anagram(s) are:"
+#define STR_ANAGRAM " %s"
 
-#define STR_USAGE_SHORT "Try '%s --help' for more information.\n"
+#define STR_USAGE_SHORT "Try '%s -h' for more information.\n"
 #define STR_USAGE_LONG "usage: %s [-d dict_name [-o db_dest]] | [-s db_src] "\
   "| [-h]\n"\
   "Interactive anagram search.\n"\
@@ -23,7 +26,7 @@
   "\t-d <dict_name>\t source dictionary for database creation.\n"\
   "\t-o <db_dest>\t custom database file name.\n"\
   "\nFind Anagrams:\n"\
-  "\t-s <db_src>\t path to custom source database file.\n"\
+  "\t-s <db_src>\t path to custom source database file.\n"
 
 /* Error strings */
 #define STR_ERR_DB_WRITE "Error writing word \"%s\" to database.\n"
