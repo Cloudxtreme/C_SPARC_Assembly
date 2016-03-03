d83b0590808c1c4c2947bee06eb34c098cc62e74
Moiz Qureshi <moquresh@ucsd.edu>
2016-03-02 17:56:07 -0800

    unit_tests/teststrIgnoreCaseCmp.c - added some test cases, try some more

--- unit_tests/teststrIgnoreCaseCmp.c
+++ unit_tests/teststrIgnoreCaseCmp.c
@@ -22,6 +22,23 @@ static void teststrIgnoreCaseCmp() {
   TEST( strIgnoreCaseCmp( rhs, lhs, n ) == -1 );
 
   /* TODO Come up with more test cases */
+  
+  lhs = "abc";
+  rhs = "ab";
+  TEST( strIgnoreCaseCmp( lhs, rhs, n ) == 1 );
+  TEST( strIgnoreCaseCmp( rhs, lhs, n ) == -1 );
+  
+  lhs = "Hellz";
+  rhs = "hella";
+  TEST( strIgnoreCaseCmp( lhs, rhs, 5 ) == 1 );
+  TEST( strIgnoreCaseCmp( rhs, lhs, 5 ) == -1 );
+
+  lhs = "lexicographical order";
+  rhs = "lexicographical order";
+  n = strlen(lhs);
+  TEST( strIgnoreCaseCmp( lhs, rhs, n ) == 0 );
+  
+
 }
 
 int main() 
