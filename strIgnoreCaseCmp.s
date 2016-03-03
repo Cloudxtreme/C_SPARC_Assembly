7d8323d90e718dcb06febab187f8c92154f7fe1c
Moiz Qureshi <moquresh@ucsd.edu>
2016-03-02 17:55:38 -0800

    strIgnoreCaseCmp.s - implemented, tested and working, need to comment

--- strIgnoreCaseCmp.s
+++ strIgnoreCaseCmp.s
@@ -8,17 +8,17 @@
  * Sources of Help: PA4 Assignment Sheet, PA4 Discussion Slides pt.1
  */
 
-	.global strIgnoreCaseCompare 	! Declare the symbol to be globally
+	.global strIgnoreCaseCmp 	! Declare the symbol to be globally
 					! visible, so we can call this function
 					! from other modules
 
 	.section ".text"		! The text segment begins here
 
 /*
- * Function name: strIgnoreCaseComp()
+ * Function name: strIgnoreCaseCmp()
  *	
  * Function prototype: 
- *  	int strIgnoreCaseComp( const char *s1, const char *s2 unsigned int n) ;
+ *  	int strIgnoreCaseCmp( const char *s1, const char *s2 unsigned int n) ;
  *
  * Description: Compares two strings char by char up to n chars ignoring case.
  * 		Uses tolower() to make each character all lower case when 
@@ -45,14 +45,86 @@
  * 	%i1 - const char *s2 - string 2
  * 	%i2 - unsigned int n - index to compare up to
  * 
+ *	%l0 - copy of %i2 -- unsigned int n
+ *	%l1 - copy of %i0 -- string 1
+ *	%l2 - copy of %i1 -- string 2
+ *	%l3 - converted lowercase char from string 1
+ *	%l4 - converted lowercase char from string 2
+ *	%l5 - holds
  */
 
-strIgnoreCaseCompare:
-	save	-96, %sp, -96	! Save the caller's window; if differen than 
+strIgnoreCaseCmp:
+	save	 %sp, -96, %sp	! Save the caller's window; if differen than 
 				! -96, then comment on how that value was 
 				! calculated.
-
 	
+	mov	%i0, %l1
+	mov 	%i1, %l2
+	mov	%i2, %l0
+	
+	cmp 	%l0, 0
+	be	endloop
+	nop
 
+loop:
+	dec 	%l0
+	
+	ldub	[%l1], %l3
+	mov	%l3, %o0
+	call 	tolower
+	nop
+	mov	%o0, %l3
+
+	ldub	[%l2], %l4
+	mov	%l4, %o0
+	call	tolower
+	nop
+	mov	%o0, %l4
+
+	cmp	%l3, %l4
+	be	endif
+	nop
+
+endif:
+	inc	%l1
+	inc	%l2
+
+	cmp	%l0, 0
+	bne	loop
+	nop
+
+checkChars:
+	sub	%l3, %l4, %l5
+	
+	cmp	%l5, 0
+	bg	positive 
+	nop
+	
+	cmp	%l5, 0
+	bl	negative
+	nop
+
+	ba	equal
+	nop
+
+positive:
+	mov 	1, %i0
+	ba	endloop
+	nop
+
+negative:
+	mov	-1, %i0
+	ba 	endloop
+	nop
+	
+equal:
+	mov	0, %i0
+	ba 	endloop
+	nop
+
+
+endloop:
+	ret	
+	restore
 
 
