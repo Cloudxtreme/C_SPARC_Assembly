d379b313928bca5f9f95ffdacb7126b675b0148c
Moiz Qureshi <moquresh@ucsd.edu>
2016-03-07 11:31:22 -0800

    printResults.c - incorrectly had summing of totalLines and totalEntries inside for-loop, moved them outside; fully commented, still needs to be tested with whole program, especially printing of stuff

--- printResults.c
+++ printResults.c
@@ -51,42 +51,53 @@ printResults( FILE *outFile, const struct argInfo *argInfoPtr,
               const struct uniqInfo *uniqInfoPtr ) {
 
   /* Local Variables */
-  struct timeval tm;
+  struct timeval tm;          // timeval struct needed for pa4gettimeofday()
 
-  char time[BUFSIZ];
+  char time[BUFSIZ];          // string to hold formatted time
   
-  int countFlag = 0;
+  int countFlag = 0;          // boolean to check if count flag is present
 
-  int duplicateEntries = 0;
-  int uniqueEntries = 0;
-  int totalEntries = 0;
+  int duplicateEntries = 0;   // holds val for duplicate entries
+  int uniqueEntries = 0;      // holds val for unique entries
+  int totalEntries = 0;       // holds val for total entries
 
-  int duplicateLines = 0;
-  int uniqueLines = 0;
-  int totalLines = 0;
+  int duplicateLines = 0;     // holds val for duplicate lines
+  int uniqueLines = 0;        // holds val for unique lines
+  int totalLines = 0;         // holds val for total lines
 
-  int i;
+  int i;                      // for-loop counter
 
-
+  // Check to see if sort output flag has ben set
   if((argInfoPtr->options & FLAG_SORT_OUTPUT) == FLAG_SORT_OUTPUT) {
+    // If coutn flag has been set then qsort using compareCount, and set
+    // count flag boolean
     if((argInfoPtr->options & FLAG_COUNT) == FLAG_COUNT) {
       qsort(uniqInfoPtr->uniqPtr, uniqInfoPtr->numOfEntries, 
             sizeof(struct uniq), compareCount);
       countFlag = 1;
     } else {
+      // If coung flag has not  been set then always do qsort with compareLine
       qsort(uniqInfoPtr->uniqPtr, uniqInfoPtr->numOfEntries, 
             sizeof(struct uniq), compareLine);
     }
   }
   
+  // Iterate through the uniq struct array
   for(i = 0; i < uniqInfoPtr->numOfEntries; i++) {
+    // Use switch case to determine output mode set in argInfoPtr
     switch(argInfoPtr->outputMode) {
+      // If mode is DuplicateAll, then print each uniq struct element's dups 
+      // member in array, where dups member is != NULL
       case DuplicateAll:
         if(uniqInfoPtr->uniqPtr[i].dups != NULL) {
           (void) fprintf(outFile,"%s", uniqInfoPtr->uniqPtr[i].dups);
         }
         break;
-
+      
+      /* If mode is DuplicateOnly, then print each uniq struct element's line 
+       * member in array, were the count member is greater than 1, if count 
+       * flag boolean is set, then first print count also. 
+       */
       case DuplicateOnly:
         if(uniqInfoPtr->uniqPtr[i].count > 1) {
           if(countFlag == 1) {
@@ -97,7 +108,11 @@ printResults( FILE *outFile, const struct argInfo *argInfoPtr,
           }
         }
       break;
-
+      
+      /* If mode is Unique, then print each uniq struct element's line member 
+       * in array, where the count member is eqaul to 1, if count flag boolean 
+       * is set, then fist print count also.
+       */
       case Unique:
         if(uniqInfoPtr->uniqPtr[i].count == 1) {
           if(countFlag == 1) {
@@ -108,7 +123,11 @@ printResults( FILE *outFile, const struct argInfo *argInfoPtr,
           }
         }
         break;
-
+      
+      /* If mode is regular, then print all elements of uniq struct array's 
+       * line member, if the count flag boolean is set, then first print the 
+       * count member for each element also.
+       */
       case Regular:
         if(countFlag == 1) {
           (void) fprintf(outFile, "%d ", uniqInfoPtr->uniqPtr[i].count); 
@@ -119,26 +138,42 @@ printResults( FILE *outFile, const struct argInfo *argInfoPtr,
         break;
     }
     
+    /* For each uniq struct array element where count member is greater than 1,
+     * increment duplicateEntries, and add up all the count members to get
+     * duplicateLines.
+     */
     if(uniqInfoPtr->uniqPtr[i].count > 1) {
       duplicateEntries += 1;
       duplicateLines += uniqInfoPtr->uniqPtr[i].count;
     }
-
+    
+    /* uniqueLines is just equal to numOfEntries in uniqInfoPtr. For each uniq
+     * struct array element where count is equal to one, increment 
+     * uniqueEntries.
+     */
     uniqueLines = uniqInfoPtr->numOfEntries;
     if(uniqInfoPtr->uniqPtr[i].count == 1) {
       uniqueEntries += 1;
     }
-   
-    totalEntries = uniqueEntries + duplicateEntries;
-    totalLines = uniqueLines + duplicateLines;
   }
+  
+  // totalEntries is just the sum of unique and duplicate entries
+  totalEntries = uniqueEntries + duplicateEntries;
 
+  // totalLines is just the sum of unique and duplicate lines
+  totalLines = uniqueLines + duplicateLines;
+
+  // get epoch time by calling pa4gettimeofday and passing in a struct timeval
   pa4gettimeofday(&tm, NULL);
+
+  // format the time using localtime(), use strftime to store into string time
   strftime(time, BUFSIZ, STR_TIME_FORMATER, localtime(&tm.tv_sec)); 
   
-  (void) fprintf(outFile, STR_PRINT_SUMMARY, duplicateEntries, uniqueEntries,
-                 totalEntries, duplicateLines, uniqueLines, totalLines, time);
-
-
+  // If the summary flag has been set in argInfoPtr, then print summary
+  if((argInfoPtr->options & FLAG_SUMMARY) == FLAG_SUMMARY) {
+    (void) fprintf(outFile, STR_PRINT_SUMMARY, duplicateEntries, uniqueEntries,
+                   totalEntries, duplicateLines, uniqueLines, totalLines, 
+                   time);
+   }
 }
 
