97f73cfa3a69740828e4a8f5fe625ddd8173f5bd
Moiz Qureshi <moquresh@ucsd.edu>
2016-03-08 12:17:45 -0800

    pa4Globals.c - added EXIT_FAILURE and EXIT_SUCCESS constants

--- pa4Globals.c
+++ pa4Globals.c
@@ -1,10 +1,11 @@
 /*
  * Filename: pa4Globals.c
- * Author: TODO
- * UserId: TODO
+ * Author: Moiz Qureshi
+ * UserId: cs30xix
  * Description: Global variables for assembly files.
  */
 #include <stdio.h> // stdout
+#include <stdlib.h>
 #include <stddef.h> //offsetof
 
 #include "pa4.h"
@@ -20,6 +21,9 @@ FILE *StandardOut = stdout;
 
 int ErrorInfoErrorCodeOffset = offsetof(struct errorInfo, errorCode);
 int ArgInfoOptionsOffset = offsetof(struct argInfo, options);
+
+int errorSuccess = EXIT_SUCCESS;
+int errorFailure = EXIT_FAILURE;
 
 /*
  * Needed for compareCount.s
