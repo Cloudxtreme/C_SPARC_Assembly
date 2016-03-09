a4f4760d5fa4cda89aec493bf57acf755642c468
Moiz Qureshi <moquresh@ucsd.edu>
2016-03-08 20:10:50 -0800

    pa4gettimeofday.c - added function header

--- pa4gettimeofday.c
+++ pa4gettimeofday.c
@@ -7,6 +7,25 @@
 
 #include <sys/time.h>
 
+/*
+ * Function Name: pa4gettimeofday()
+ *
+ * Function Prototype: 
+ *  int pa4gettimeofday( struct timeval *tp, struct timezone *tzp );
+ *
+ * Description: Gets the epoch time in seconds since 1/1/70 and stores it
+ *              into struct timeval *tp.
+ *
+ * Parameters: 
+ *    arg 1 - struct timeval *tp -- holds epoch time in seconds
+ *    arg 2 - struct timezone *tzp -- used for timezone, use NULL here though
+ *
+ * Side Effects: Gets epoch time in seconds and stores in struct timeval *tp
+ *
+ * Error Conditions: None
+ *
+ * Return Values: int, but will (void) return value
+ */
 int pa4gettimeofday( struct timeval *tp, struct timezone *tzp ) {
   return gettimeofday( tp, tzp );
 }
